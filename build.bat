c59df0dfb055b1c55c63b3e001ef6f7e3ab30249
John Bork <jlbork@mtu.edu>
2020-09-28 22:03:42 -0500

    set linker subsystem to windows

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 mkdir build\win32
 pushd build\win32
-cl /DWIN32 /Zi /Fewin32_doom ..\..\win32\win32_doom.c ..\..\win32\win32_layer.c ..\..\win32\*.c user32.lib gdi32.lib ws2_32.lib
+cl /DWIN32 /Zi /Fewin32_doom ..\..\win32\win32_doom.c ..\..\win32\win32_layer.c ..\..\win32\*.c /link /SUBSYSTEM:WINDOWS user32.lib gdi32.lib ws2_32.lib
 popd
